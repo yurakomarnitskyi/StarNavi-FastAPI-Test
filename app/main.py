9624aee9b58b572809f0102c5bf25edd3caf09b5
Yura <komarnickijura@gmail.com>
2024-03-15 10:14:24 +0200

    test update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
+"""
+Main file run project and inclide all routes.
+"""
 from fastapi import FastAPI
 from workflow import routes as workflow_routes
 from node import routes as node_routes
 from edge import routes as edge_routes
 
+
 app = FastAPI()
+
 
 app.include_router(workflow_routes.router)
 app.include_router(node_routes.router)
